2d6241caf342ad09831da13556b915fb8f1014c3
Rybar <98572221+Rybarrr@users.noreply.github.com>
2023-12-10 12:57:50 +0800

    Update proc.c
    4.2

--- lab4/arch/riscv/kernel/proc.c
+++ lab4/arch/riscv/kernel/proc.c
@@ -20,46 +20,42 @@ struct task_struct* task[NR_TASKS]; // 线程数组, 所有的线程都保存在
 //extern uint64 task_test_priority[]; // test_init 后, 用于初始化 task[i].priority 的数组
 //extern uint64 task_test_counter[];  // test_init 后, 用于初始化 task[i].counter  的数组
 
+void initialize_process(struct task_struct* process, int pid) {
+    process->pid = pid;
+    process->state = TASK_RUNNING;
+    process->counter = (pid == 0) ? 0 : task_test_counter[pid];
+    process->priority = (pid == 0) ? 0 : task_test_priority[pid];
+    process->thread.ra = (uint64)&__dummy;
+    process->thread.sepc = USER_START;
+    process->thread.sstatus = 0x40020; // SPP=0, SPIE=1, SUM=1
+    process->thread.sscratch = USER_END;
+    process->thread.sp = (uint64)process + PGSIZE;
+    process->pgd = (pagetable_t)(alloc_page() - PA2VA_OFFSET);
+    memcpy((void*)((uint64)process->pgd + PA2VA_OFFSET), (void*)swapper_pg_dir, PGSIZE);
+    // 如果有其他需要初始化的字段，也可以在这里添加
+}
+
 void task_init() {
-    //test_init(NR_TASKS);
-    // 1. 调用 kalloc() 为 idle 分配一个物理页
-    // 2. 设置 state 为 TASK_RUNNING;
-    // 3. 由于 idle 不参与调度 可以将其 counter / priority 设置为 0
-    // 4. 设置 idle 的 pid 为 0
-    // 5. 将 current 和 task[0] 指向 idle
+    test_init(NR_TASKS);
     idle = (struct task_struct*)kalloc();
-    idle->state = TASK_RUNNING;
-    idle->counter = 0;
-    idle->pid = 0;
-    current = task[0] = idle;
+    initialize_process(idle, 0); // 初始化 idle 进程
+    current = idle;
+    task[0] = idle;
 
-    // 1. 参考 idle 的设置, 为 task[1] ~ task[NR_TASKS - 1] 进行初始化
-    // 2. 其中每个线程的 state 为 TASK_RUNNING, 此外，为了单元测试的需要，counter 和 priority 进行如下赋值：
-    //      task[i].counter  = task_test_counter[i];
-    //      task[i].priority = task_test_priority[i];
-    // 3. 为 task[1] ~ task[NR_TASKS - 1] 设置 `thread_struct` 中的 `ra` 和 `sp`,
-    // 4. 其中 `ra` 设置为 __dummy （见 4.3.2）的地址,  `sp` 设置为 该线程申请的物理页的高地址
-    for(int i = 1;i < NR_TASKS;i++){
-        task[i] = (struct task_struct*)kalloc(); 
-        task[i]->state = TASK_RUNNING;
-        task[i]->counter = 0; 
-        task[i]->priority = rand(); 
-        task[i]->pid = i;
-        task[i]->thread.ra = (uint64)&__dummy;
-        task[i]->thread.sp = (uint64)task[i] + PGSIZE;
+    for (int i = 1; i < NR_TASKS; ++i) {
+        task[i] = (struct task_struct*)kalloc();
+        initialize_process(task[i], i); // 初始化其他进程
+#ifdef SJF
+        printk("SET [PID = %d COUNTER = %d]\n", i, task_test_counter[i]);
+#endif
+#ifdef PRIORITY
+        printk("SET [PID = %d PRIORITY = %d COUNTER = %d]\n", i, task_test_priority[i], task_test_counter[i]);
+#endif
     }
-    #define OFFSET(TYPE , MEMBER) ((unsigned long)(&(((TYPE *)0)->MEMBER)))
-
-    const uint64 OffsetOfThreadInTask = (uint64)OFFSET(struct task_struct, thread);
-    const uint64 OffsetOfRaInTask = OffsetOfThreadInTask+(uint64)OFFSET(struct thread_struct, ra);
-    const uint64 OffsetOfSpInTask = OffsetOfThreadInTask+(uint64)OFFSET(struct thread_struct, sp);
-    const uint64 OffsetOfSInTask = OffsetOfThreadInTask+(uint64)OFFSET(struct thread_struct, s);
-    printk("%d\n",OffsetOfRaInTask);
-    printk("%d\n",OffsetOfSpInTask);
-    printk("%d\n",OffsetOfSInTask);
 
     printk("...proc_init done!\n");
 }
+
 
 // arch/riscv/kernel/proc.c
 void dummy() {
@@ -84,12 +80,13 @@ void dummy() {
 extern void __switch_to(struct task_struct* prev, struct task_struct* next);
 
 void switch_to(struct task_struct* next) {
-    if (current == next)
+    if(next->pid == current->pid) {
         return;
-    else{
-        struct task_struct *prev = current;
+    } else {
+        struct task_struct* old_current = current;
         current = next;
-        __switch_to(prev,next);
+        uint64 next_satp = (((uint64)(next->pgd))>>12) | (1L << 63);
+        __switch_to(old_current, next, next_satp);
     }
 }
 
